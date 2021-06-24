a9d4294ff6785667e54dd0256e840fd7c49f4a30
Robin Fournier <robin@imatt.ch>
2021-06-24 14:40:35 +0200

    Allows to get status + generate app_password

--- lib/nextcloud/ocs_api.rb
+++ lib/nextcloud/ocs_api.rb
@@ -1,5 +1,6 @@
 module Nextcloud
   class OcsApi < Api
+    include Helpers
     # Initiates User class
     #
     # @param userid [Ingteger,nil] Nextcloud user userid
@@ -31,11 +32,30 @@ module Nextcloud
       Ocs::FileSharingApi.new(url: @url, username: @username, password: @password)
     end
 
+    def generate_app_password
+      tmp_url = @url
+      @url = URI(@url.scheme + "://" + @url.host + "/ocs/v2.php/core/")
+      response = request(:get, "getapppassword")
+      meta = get_meta(response)
+
+      @url = tmp_url
+
+      if meta && meta['statuscode'] && meta['statuscode'] == "200"
+        return "#{response.xpath('//data/apppassword/text()')}"
+      else
+        throw "Error while getting app password : #{meta['message'] || 'unkown'}"
+      end
+    end
+
     # Initiates Group Folder class
     #
     # @return [Object] Group Folder instance
     def group_folder
       Ocs::GroupFolder.new(url: @url, username: @username, password: @password)
+    end
+
+    def status
+      get_meta(request(:get, "users/#{@username}"))
     end
   end
 end
